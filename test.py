1d559d608fcdd14251745c39f2fa1c2d1a5aa468
tahsi <tahsin16@cse.pstu.ac.bd>
2023-08-13 05:22:43 +0600

    inventory update fix

--- test.py
+++ test.py
@@ -4,21 +4,21 @@ import random
 order_cost = 100
 holding_cost = 5
 initial_inventory_level = 8
-demand_distribution = [10, 20, 30, 40, 50]  # Example demand distribution
-lead_time_distribution = [2, 3, 4, 5]  # Example lead time distribution
+demand_distribution = [0, 1, 2, 3, 4]  # Example demand distribution
+lead_time_distribution = [1, 2, 3, 4, 5]  # Example lead time distribution
 
 
 def simulate_inventory_system(order_quantity, reorder_point, num_periods=12):
     total_orders_placed = 0
     total_ordering_cost = 0
     total_holding_cost = 0
-    total_orders_lost = 0
+    total_sales_lost = 0
     inventory_level = initial_inventory_level
     total_cost = 0
 
     for period in range(num_periods):
         # Check if an order needs to be placed
-        if inventory_level <= reorder_point:
+        if period == 0 or inventory_level <= reorder_point:
             order = order_quantity
             total_orders_placed += 1
             total_ordering_cost += order_cost
@@ -27,30 +27,28 @@ def simulate_inventory_system(order_quantity, reorder_point, num_periods=12):
 
         # Simulate demand and lead time
         demand = random.choice(demand_distribution)
-        lead_time = random.choice(lead_time_distribution)
 
-        # Update inventory levels
-        if inventory_level >= demand:
+        # Update inventory levels based on demand and order
+        if demand <= inventory_level:
             inventory_level -= demand
             total_holding_cost += inventory_level * holding_cost
-        else:
+        elif demand > inventory_level:
+            lead_time = random.choice(lead_time_distribution)
+            inventory_level = inventory_level
             total_holding_cost += inventory_level * holding_cost
-            inventory_level = 0
 
             # Check if stockout occurred during lead time
-            if period <= lead_time:
+            if period < lead_time:
                 inventory_level += 5
 
-            if inventory_level < demand:
-                total_orders_lost += demand
-
+        # Calculate costs
         total_cost += order_cost + total_holding_cost
 
     return {
         "total_orders_placed": total_orders_placed,
         "total_ordering_cost": total_ordering_cost,
         "total_holding_cost": total_holding_cost,
-        "total_orders_lost": total_orders_lost,
+        "total_sales_lost": total_sales_lost,
         "total_cost": total_cost
     }
 
@@ -76,4 +74,4 @@ print(f"Minimum Total Cost: {min_total_cost:.2f}")
 print(f"Number of Orders Placed: {best_results['total_orders_placed']}")
 print(f"Total Ordering Cost: {best_results['total_ordering_cost']:.2f}")
 print(f"Total Holding Cost: {best_results['total_holding_cost']:.2f}")
-print(f"Total Orders Lost: {best_results['total_orders_lost']}")
+#print(f"Total Sales Lost: {best_results['total_sales_lost']}")
